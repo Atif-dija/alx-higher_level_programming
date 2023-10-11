7eecb630b88671b9549fd34f942ae5a7abe36e04
Atif-dija <khadijaaif2@gmail.com>
2023-10-11 09:39:22 -0700

    Update dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     matrix1 = matrix.copy()
     for j in range(len(matrix)):
         matrix1[j] = list(map(lambda a: a**2, matrix[j]))
-    return (matrix1)
+    return matrix1
