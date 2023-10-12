dff26922de47a03ebbafaf2f99e47483e8c37ccc
Atif-dija <khadijaaif2@gmail.com>
2023-10-11 19:01:09 -0700

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    num = 0
+    T = 0
+    dgts = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for s in reversed(roman_string):
+        num = dgts[s]
+        if T < num * 5:
+            T+= num
+        else:
+            T-= num
+    return T
