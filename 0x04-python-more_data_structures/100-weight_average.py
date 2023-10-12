8662d9bb1c33f27bc1a1800480eb18e6724716fb
Atif-dija <khadijaaif2@gmail.com>
2023-10-11 17:29:09 -0700

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,4 +3,9 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     else:
-        return (sum(a *b for a, b in my_list) / sum(b for a, b in my_list))
+        num1 = 0
+        num2 = 0
+        for tuple in my_list:
+            num1 += tuple[0] * tuple[1]
+            num2 += tuple[1]
+        return (num1 / num2)
